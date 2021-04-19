9ec2a5b993aedaf0fcdb56098e708b3399a7ad9a
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-18 21:32:20 -0700

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -1,9 +1,11 @@
 from ssg import hooks, parsers
 
+
 files = []
 
+
 @hooks.register("collect_files")
-def collect_files(source, site_parser):
+def collect_files(source, site_parsers):
 	valid = lambda p: not isinstance(p, parsers.ResourceParser)
 	for path in source.rglob("*"):
 		for parser in list(filter(valid, site_parsers)):
@@ -13,6 +15,6 @@ def collect_files(source, site_parser):
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
 	template = '<li><a href="{}{}">{}</a></li>'
-	menu_item = lambda name, ext: format(template, name, ext, name.title())
+	menu_item = lambda name, ext: template.format(name, ext, name.title())
 	menu = "\n".join([menu_item(path.stem, ext) for path in files])
-	return "<ul>\n{}<ul>\n{}", format(menu, html)
+	return "<ul>\n{}<ul>\n{}".format(menu, html)
