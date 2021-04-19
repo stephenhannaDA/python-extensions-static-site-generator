648d2ef8c6d7c4855feb213c03c25909d218c8cb
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-18 19:32:02 -0700

    module1

---
+++ ssg/extensions.py
@@ -0,0 +1,20 @@
+import sys
+import importlib
+
+from pathlib import Path
+
+def load_module(directory, name):
+	sys.path.insert(directory)
+	importlib.import_module(name)
+	sys.path.pop(0)
+
+def load_directory(directory):
+	for path in directory.rglob(".py"):
+		load_module(directory.as_posix(), path.stem)
+
+def load_bundled():
+	dirctory = Path(__file__).parent/"extensions"
+	load_directory(directory)
+
+
+
